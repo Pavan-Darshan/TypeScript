fe6d9fca4fb50dd0c0b6415ba05f59a5d8a191a3
Pavan Darshan <pavan.bhuvaneshwar@itorizon.com>
2025-01-06 11:30:55 +0530

    6/1/2025

---
+++ test.ts
@@ -0,0 +1,84 @@
+// function getProduct(id){
+//   return {
+//     id: id,
+//     name: `Awesome Gadget ${id}`,
+//     price: 99.5
+//   }
+// }
+
+// const product = getProduct(1);
+// console.log(`The product ${product.name} costs $${product.price}`);
+
+
+
+// interface Product{
+//   id : Number,
+//   name:String,
+//   price:Number
+// }
+
+// function getProduct(id): Product{
+//   return {
+//     id: id,
+//     name: `Awesome Gadget ${id}`,
+//     price: 99.5
+//   }
+// }
+
+// const pro = getProduct(101);
+
+// console.log("ID :",pro.id," Name :",pro.name)
+
+
+
+
+// let person :{
+//   name :string;
+//   id:number;
+// };
+
+// person={name:'pavan',id:"ppp"};
+
+// console.log(person)
+
+
+// function changeStatus(status: boolean): boolean {
+//   return false;
+// }
+
+// console.log(changeStatus(10))
+
+
+// let employee:{
+//     name:string;
+//     age:number;
+//     mob:number;
+//   };
+  
+//   employee={name:"pavan",age:29,mob:959585};
+  
+//   console.log(employee)
+  
+//   let arr:string[] = [];
+  
+  
+//   let skill: [string, number?];
+//   skill = ['Programming'];
+  
+  
+//   console.log(skill)
+  
+//   let a:number[]=[1,2,3];
+  
+//   let res=a.reduce((t,v)=>{ return t+v})
+//   console.log(res)
+  
+  
+//   let b:any;
+  
+//   b=12;
+//   console.log(b)
+
+let num : string;
+num ="102";
+console.log(num);
